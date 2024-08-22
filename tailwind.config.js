616ea3759fc87b864ae7288aa904b495954d3ed5
Abdulhadi46 <abdulhadi200205@gmail.com>
2024-08-22 19:21:28 +0500

    add about section

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,8 +24,27 @@ module.exports = {
     },
     extend: {
       screens: {
-        "2sm": "120px",
-        "3sm": "500px",
+        "2sm": "300px",
+        "3sm": "250px",
+      },
+      animation: {
+        "spin-slow": "spin 6s linear infinite",
+      },
+      keyframes: {
+        wiggle: {
+          "0%": { transform: "translateY(0%)" },
+          "25%": { transform: "translateY(-100%)" },
+          "50%": { transform: "translateY(-200%)" },
+          "75%": { transform: "translateY(-100%)" },
+          "100%": { transform: "translateY(0%)" },
+        },
+      },
+      colors: {
+        dark_blue: "#130F49",
+        light_purple: "#A5A6FF",
+        backGround_color: "#EFFBF8",
+        textColor: "#55527C",
+        hoverColor: "#FF972D",
       },
     },
   },
